863065426847ba04d7a9a565808c0bfdf82c058e
Erin W Dickie <erin.w.dickie@gmail.com>
2015-09-01 15:52:18 -0400

    the cleaner version

--- get_func_jiggy.py
+++ get_func_jiggy.py
@@ -3,15 +3,18 @@
 This looks for most correlated seeds within brain networks
 
 Usage:
-  get_func_jiggy.py [options] <input.dtseries.nii> <ROItemplate.dscalar.nii>
+  get_func_jiggy.py [options] <input.dtseries.nii> <ROItemplate.dscalar.nii> <result.csv>
 
 Arguments:
     <input.dtseries.nii>        Paths to directory containing .mnc images to align and mean
-    <ROItemplate.dscalar.nii>  Path to template for the ROIs of network regions
+    <ROItemplate.dscalar.nii>   Path to template for the ROIs of network regions
+    <result.csv>                Name (with full path to the csv of the found nodes)
+
 Options:
   -v,--verbose             Verbose logging
   --debug                  Debug logging in Erin's very verbose style
   -n,--dry-run             Dry run
+  -h,--help                Print help 
 
 DETAILS
 Requires python enviroment with numpy and nibabel:
@@ -38,6 +41,7 @@ import pandas as pd
 arguments       = docopt(__doc__)
 inputfile       = arguments['<input.dtseries.nii>']
 ROItemplate     = arguments['<ROItemplate.dscalar.nii>']
+resultsfile     = arguments['<result.csv>']
 VERBOSE         = arguments['--verbose']
 DEBUG           = arguments['--debug']
 DRYRUN          = arguments['--dry-run']
@@ -61,13 +65,12 @@ if os.path.exists(sub_dconn)==False:
 ## read in the dconn file
 img1 = nib.load(sub_dconn)
 sub_corrmat = img1.get_data()
-shape(sub_corrmat)
-dconn=sub_corrmat[0,0,0,0,:,:]
 fc_df = pd.DataFrame(sub_corrmat[0,0,0,0,:,:])
+
 ## also read in template
 img2 = nib.load(ROItemplate)
-ROInet2 = img2.get_data()
-roi_df = pd.DataFrame(ROInet2[0,0,0,0,:,:],index=range(1,shape(ROInet2)[4]+1))
+ROInet = img2.get_data()
+roi_df = pd.DataFrame(ROInet[0,0,0,0,:,:],index=range(1,ROInet.shape[4]+1))
 
 
 cols = ['Network','ROI','cifti-index']
@@ -76,9 +79,6 @@ thisrow = 1
 for network in roi_df.index.values.tolist():
     ### subset matrix to contain rows of only this Network
     network_idx = roi_df.loc[network,roi_df.loc[network,:]>0].index
-    #network_mat = fc_df.loc[network_idx,network_idx]
-    ### also subset template ROI matrix to get one network
-    #net_rois = roi_df.loc[network,network_idx]
     ### get a list of ROIs to within the network
     roi_list = roi_df.loc[network,network_idx].unique()
     for roi in roi_list:
@@ -87,9 +87,7 @@ for network in roi_df.index.values.tolist():
         '''
         ### get the subarray that contains the ROI
         roi_idx = roi_df.loc[network,roi_df.loc[network,:]==roi].index
-        #roi_mat = fc_df.loc[roi_idx,:]
         ### calculate the maximal correlation at every vertex with this ROI
-        #roi_maxified = roi_mat.max()
         max_indexes = []
         for other_roi in roi_list[roi_list != roi]:
             ## for combination of ROI and other_roi get the indices of the other roi that is most correlated with it
@@ -112,10 +110,5 @@ for network in roi_df.index.values.tolist():
         thisrow = thisrow + 1
 
 
-
-
-
-
-
-
-## take median of max correlation values within each ROI within each network
+## write the checklist out to a file
+result.to_csv(resultsfile, sep=',', columns = cols, index = False)
